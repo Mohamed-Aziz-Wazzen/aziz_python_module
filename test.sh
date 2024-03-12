dd7b778a1690fbd19beaac92216247f110fa8f06
MedAziz <135025588+Mohamed-Aziz-Wazzen@users.noreply.github.com>
2024-03-12 16:05:58 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
 
-# Récupérer le répertoire du script
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-
 # Vérifier la présence des fichiers nécessaires
-if [ ! -f "$SCRIPT_DIR/../setup.py" ]; then
+if [ ! -f "./setup.py" ]; then
   echo "Le fichier setup.py est manquant."
   exit 1
 fi
 
-if [ ! -f "$SCRIPT_DIR/../azizmodule/__init__.py" ]; then
+if [ ! -f "./azizmodule/__init__.py" ]; then
   echo "Le fichier __init__.py est manquant."
   exit 1
 fi
 
-if [ ! -f "$SCRIPT_DIR/../azizmodule/azizmodule.py" ]; then
+if [ ! -f "./azizmodule/azizmodule.py" ]; then
   echo "Le fichier azizmodule.py est manquant."
   exit 1
 fi
 
 # Afficher un message de réussite
 echo "La structure de votre module semble correcte."
+
 
 
 
